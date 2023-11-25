62095d6ec5fcee4b39d42b67bf87d14ec511a19b
Ali Özcan <ali-ozcan93@hotmail.com>
2023-11-25 21:16:57 +0300

    In Core.Persistence, added Entity and Audit classes

---
+++ Core.Persistence/Repositories/Audit.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.Persistence.Repositories;
+
+public class Audit<TId>
+{
+    public DateTime CreatedDate { get; set; }
+    public TId CreatedUserId { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public TId? UpdatedUserId { get; set; }
+    public DateTime? DeletedTime { get; set; }
+    public TId? DeletedUserId { get; set; }
+
+    public Audit()
+    {
+        CreatedDate = default;
+        CreatedUserId = default;
+    }
+
+    public Audit(TId createdUserId, TId? updatedUserId, TId? deletedUserId)
+    {
+        CreatedUserId = createdUserId;
+        UpdatedUserId = updatedUserId;
+        DeletedUserId = deletedUserId;
+    }
+}
---
+++ Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Persistence.Repositories;
+
+public class Entity<TId> : Audit<TId>
+{
+    public TId Id { get; set; }
+
+    public Entity()
+    {
+        Id = default;
+    }
+
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
