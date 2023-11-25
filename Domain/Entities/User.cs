47485df9538c41ba208e1479092fbd1a1bf7e177
Ali Özcan <ali-ozcan93@hotmail.com>
2023-11-25 21:17:52 +0300

    In Domain.Entity, added User class

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class User : Entity<Guid>
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Platform { get; set; }
+
+    public User()
+    {
+        
+    }
+
+    public User(Guid id, string name, string email, string platform)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Platform = platform;
+    }
+}
