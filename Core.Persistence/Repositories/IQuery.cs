ffd34c711a107fa6e8139804f71a66861ba8b2d6
Ali Özcan <ali-ozcan93@hotmail.com>
2023-11-26 13:17:07 +0300

    In Core.Persistence.Repositories, added parameters to IQuery's Query method.

--- Core.Persistence/Repositories/IQuery.cs
+++ Core.Persistence/Repositories/IQuery.cs
@@ -1,6 +1,16 @@
-﻿namespace Core.Persistence.Repositories;
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Core.Persistence.Repositories;
 
 public interface IQuery<T>
 {
-    IQueryable<T> Query();
+    IQueryable<T> Query
+        (
+            Expression<Func<T, bool>>? filter = null, 
+            Func<IQueryable<T>, IIncludableQueryable<T,object>>? include = null, 
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            bool withDeleted = false, 
+            bool enableTracking = true
+        );
 }
