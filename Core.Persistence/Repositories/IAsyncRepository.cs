c32191e6f28648a24ff1626646161637c77529f7
Ali Özcan <ali-ozcan93@hotmail.com>
2023-11-26 12:19:05 +0300

    In Core.Persistence.Reponsitories, added sync and async repository interfaces.

---
+++ Core.Persistence/Repositories/IAsyncRepository.cs
@@ -0,0 +1,42 @@
+﻿using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Core.Persistence.Repositories;
+
+public interface IAsyncRepository<TEntity, TEntityIdType> : IQuery<TEntity>
+    where TEntity : Entity<TEntityIdType>
+{
+    Task<TEntity?> GetAsync
+        (
+            Expression<Func<TEntity, bool>>? filter = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+            bool withDeleted = false,
+            bool enableTracking = true,
+            CancellationToken cancellationToken = default
+        );
+    Task<Paginate<TEntity>> GetListAsync
+        (
+            Expression<Func<TEntity, bool>>? filter = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+            int index = 0,
+            int size = 10,
+            bool withDeleted = false,
+            bool enableTracking = true,
+            CancellationToken cancellationToken = default
+        );
+    Task<bool> AnyAsync
+        (
+            Expression<Func<TEntity, bool>>? filter = null,
+            bool withDeleted = false,
+            bool enableTracking = true,
+            CancellationToken cancellationToken = default
+        );
+    Task<TEntity> AddAsync(TEntity entity);
+    Task<ICollection<TEntity>> AddRangeAsync(ICollection<TEntity> entities);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task<ICollection<TEntity>> UpdateRangeAsync(ICollection<TEntity> entities);
+    Task<TEntity> DeleteAsync(TEntity entity, bool permanent = false);
+    Task<ICollection<TEntity>> DeleteRangeAsync(ICollection<TEntity> entities, bool permanent = false);
+}
---
+++ Core.Persistence/Repositories/IRepository.cs
@@ -0,0 +1,39 @@
+﻿using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Core.Persistence.Repositories;
+
+public interface IRepository<TEntity, TEntityIdType> : IQuery<TEntity>
+    where TEntity : Entity<TEntityIdType>
+{
+    TEntity? Get
+        (
+            Expression<Func<TEntity, bool>>? filter = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+            bool withDeleted = false,
+            bool enableTracking = true
+        );
+    Paginate<TEntity> GetList
+        (
+            Expression<Func<TEntity, bool>>? filter = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+            int index = 0,
+            int size = 10,
+            bool withDeleted = false,
+            bool enableTracking = true
+        );
+    bool? Any
+        (
+            Expression<Func<TEntity, bool>>? filter = null,
+            bool withDeleted = false,
+            bool enableTracking = true
+        );
+    TEntity Add(TEntity entity);
+    ICollection<TEntity> AddRange(ICollection<TEntity> entities);
+    TEntity Update(TEntity entity);
+    ICollection<TEntity> UpdateRange(ICollection<TEntity> entities);
+    TEntity Delete(TEntity entity, bool permanent = false);
+    ICollection<TEntity> DeleteRange(ICollection<TEntity> entities, bool permanent = false);
+}
