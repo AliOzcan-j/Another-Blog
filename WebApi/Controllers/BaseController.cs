c927e44a0a76ffbca90ffe76217634531acf495a
Ali Özcan <ali-ozcan93@hotmail.com>
2023-12-02 15:03:49 +0300

    In WebApi, added the BaseController and UserController

---
+++ WebApi/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        // This line checks if _mediator is set and if it is, return that, 
+        // if it's not then look for an IMediator in injections and set it and return
+        protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.Users.Commands;
+using Application.Features.Users.Dtos;
+using Application.Features.Users.Queries.GetList;
+using Core.Application.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateUserCommand createUserCommand)
+        {
+            CreatedUserResponseDto response = await Mediator.Send(createUserCommand);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            GetListUserQuery getListUserQuery = new();
+            GetListResponse<GetListUserListItemDto> response = await Mediator.Send(getListUserQuery);
+            return Ok(response);
+        }
+    }
+}
