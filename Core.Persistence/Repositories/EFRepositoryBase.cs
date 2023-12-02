3d8c7fe7cd04069ac794447a631550fb7ae8497a
Ali Özcan <ali-ozcan93@hotmail.com>
2023-12-02 22:10:09 +0300

    In Core.Persistence.Repositories EFRepositoryBase, added summaries to asynchronous CRUD methods.

--- Core.Persistence/Repositories/EFRepositoryBase.cs
+++ Core.Persistence/Repositories/EFRepositoryBase.cs
@@ -17,23 +17,80 @@ public class EFRepositoryBase<TEntity, TEntityIdType, TContext>(TContext context
     protected readonly TContext Context = context;
 
     #region Async
+    /// <summary>
+    ///     <b>Asynchronously</b> performs <see cref="EntityFrameworkCore"/> operations to <b>Add</b> 
+    ///     an <paramref name="entity"/> to connected database while also assigning the create date of the record
+    /// </summary>
+    /// 
+    /// <param name="entity">
+    ///     Represents the entity with the identity type of <see cref="TEntityIdType"/> 
+    ///     on which the operation will be performed.
+    /// </param>
+    /// 
+    /// <returns>
+    ///     A task that represents the asynchronous Add operation. 
+    ///     The task result contains the entity on which the operation was performed.
+    /// </returns>
     public async Task<TEntity> AddAsync(TEntity entity)
     {
+        //will be removed when Audit operation is implemented
         entity.CreatedDate = DateTime.Now;
         await Context.AddAsync(entity);
         await Context.SaveChangesAsync();
         return entity;
     }
 
+    /// <summary>
+    ///     <b>Asynchronously</b> performs <see cref="EntityFrameworkCore"/> operations to <b>Add</b> a list of 
+    ///     <paramref name="entities"/> to connected database while also assigning the create date of the record
+    /// </summary>
+    /// 
+    /// <param name="entities">
+    ///     Represents the list of entities with the identity type of <see cref="TEntityIdType"/> 
+    ///     on which the operation will be performed.
+    /// </param>
+    ///     
+    /// <returns>
+    ///     A task that represents the asynchronous AddRange operation. 
+    ///     The task result contains the list of entities on which the operation was performed.
+    /// </returns>
     public async Task<ICollection<TEntity>> AddRangeAsync(ICollection<TEntity> entities)
     {
         foreach (var entity in entities)
             entity.CreatedDate = DateTime.UtcNow;
+        //will be switched to AddRange variant when Audit operation is implemented
         await Context.AddAsync(entities);
         await Context.SaveChangesAsync();
         return entities;
     }
 
+    /// <summary>
+    ///     <b>Asynchronously</b> determines whether a sequence contains any elements. 
+    ///     Within the method, it can accept parameters to modify the query.
+    /// </summary>
+    /// 
+    /// <param name="filter">
+    ///     Represents the linq query which will modify the default query
+    /// </param>
+    /// 
+    /// <param name="withDeleted">
+    ///     Represents the boolean flag which will determine whether to fetch soft deleted records
+    /// </param>
+    /// 
+    /// <param name="enableTracking">
+    ///     Represents the boolean flag which will determine whether to let EF Core to 
+    ///     track the entity of the current context
+    /// </param>
+    /// 
+    /// <param name="cancellationToken">
+    ///     Represents the CancellationToken wihch will be used to facilitate the cancellation of operation.
+    /// </param>
+    /// 
+    /// <returns>
+    ///     A task that represents the asynchronous AddRange operation. 
+    ///     The task result contains <see langword="true"/> if the source sequence contains 
+    ///     any elements; otherwise, <see langword="false"/>
+    /// </returns>
     public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>>? filter = null, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
     {
         IQueryable<TEntity> queryable = Query();
@@ -48,6 +105,24 @@ public class EFRepositoryBase<TEntity, TEntityIdType, TContext>(TContext context
         return await queryable.AnyAsync(cancellationToken);
     }
 
+    /// <summary>
+    ///     <b>Asynchronously</b> performs <see cref="EntityFrameworkCore"/> operations to <b>Delete</b> 
+    ///     an <paramref name="entity"/> from connected database 
+    ///     This method also calls methods to determine if the entity is valid for deleting.
+    /// </summary>
+    /// 
+    /// <param name="entity">
+    ///     Represents the entity with the identity type of <see cref="TEntityIdType"/> 
+    ///     on which the operation will be performed.
+    /// </param>
+    /// <param name="permanent">
+    ///     Represents the boolean flag which will be used to determine whether to delete the record permanently
+    /// </param>
+    /// 
+    /// <returns>
+    ///     A task that represents the asynchronous Delete operation. 
+    ///     The task result contains the entity on which the operation was performed.
+    /// </returns>
     public async Task<TEntity> DeleteAsync(TEntity entity, bool permanent = false)
     {
         await SetEntityAsDeletedAsync(entity, permanent);
@@ -55,6 +130,24 @@ public class EFRepositoryBase<TEntity, TEntityIdType, TContext>(TContext context
         return entity;
     }
 
+    /// <summary>
+    ///     <b>Asynchronously</b> performs <see cref="EntityFrameworkCore"/> operations to <b>Delete</b> 
+    ///     a list of <paramref name="entities"/> from connected database 
+    ///     This method also calls methods to determine if the entities are valid for deleting.
+    /// </summary>
+    /// 
+    /// <param name="entities">
+    ///     Represents the list of entity with the identity type of <see cref="TEntityIdType"/> 
+    ///     on which the operation will be performed.
+    /// </param>
+    /// <param name="permanent">
+    ///     Represents the boolean flag which will be used to determine whether to delete the record permanently
+    /// </param>
+    /// 
+    /// <returns>
+    ///     A task that represents the asynchronous DeleteRange operation. 
+    ///     The task result contains the list of entity on which the operation was performed.
+    /// </returns>
     public async Task<ICollection<TEntity>> DeleteRangeAsync(ICollection<TEntity> entities, bool permanent = false)
     {
         await SetEntityAsDeletedAsync(entities, permanent);
@@ -62,6 +155,38 @@ public class EFRepositoryBase<TEntity, TEntityIdType, TContext>(TContext context
         return entities;
     }
 
+    /// <summary>
+    ///     <b>Asynchronously</b> returns the first element of a sequence, 
+    ///     or a default value if the sequence contains no element
+    ///     Within the method, it can accept parameters to modify the query.
+    /// </summary>
+    /// 
+    /// <param name="filter">
+    ///     Represents the linq query which will modify the default query
+    /// </param>
+    /// 
+    /// <param name="include">
+    ///     Represents the linq query which will modify the query to include records of given related entities.
+    /// </param>
+    /// 
+    /// <param name="withDeleted">
+    ///     Represents the boolean flag which will determine whether to fetch soft deleted records
+    /// </param>
+    /// 
+    /// <param name="enableTracking">
+    ///     Represents the boolean flag which will determine whether to let EF Core to 
+    ///     track the entity of the current context
+    /// </param>
+    /// 
+    /// <param name="cancellationToken">
+    ///     Represents the CancellationToken wihch will be used to facilitate the cancellation of operation.
+    /// </param>
+    /// 
+    /// <returns>
+    ///     A task that represents the asynchronous FirstOrDefault operation. 
+    ///     The task result contains <see langword="default"/> ( <typeparamref name="TEntity"/> ) if
+    ///     the source query result is <see langword="null"/>; otherwise, the first element in sequence.
+    /// </returns>
     public async Task<TEntity?> GetFirstAsync(Expression<Func<TEntity, bool>>? filter = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
     {
         IQueryable<TEntity> queryable = Query
@@ -74,6 +199,37 @@ public class EFRepositoryBase<TEntity, TEntityIdType, TContext>(TContext context
         return await queryable.FirstOrDefaultAsync(cancellationToken);
     }
 
+    /// <summary>
+    ///     <b>Asynchronously</b> returns the only element of a sequence, or a default value if the sequence is empty; 
+    ///     this method throws an exception if there is more than one element in the sequence.
+    /// </summary>
+    /// 
+    /// <param name="filter">
+    ///     Represents the linq query which will modify the default query
+    /// </param>
+    /// 
+    /// <param name="include">
+    ///     Represents the linq query which will modify the query to include records of given related entities.
+    /// </param>
+    /// 
+    /// <param name="withDeleted">
+    ///     Represents the boolean flag which will determine whether to fetch soft deleted records
+    /// </param>
+    /// 
+    /// <param name="enableTracking">
+    ///     Represents the boolean flag which will determine whether to let EF Core to 
+    ///     track the entity of the current context
+    /// </param>
+    /// 
+    /// <param name="cancellationToken">
+    ///     Represents the CancellationToken wihch will be used to facilitate the cancellation of operation.
+    /// </param>
+    /// 
+    /// <returns>
+    ///     A task that represents the asynchronous SingleOrDefault operation. The task result contains the 
+    ///     single element of the input sequence, or <see langword="default"/> ( <typeparamref name="TEntity"/> ) 
+    ///     if the sequence contains no elements.
+    /// </returns>
     public async Task<TEntity?> GetSingleAsync(Expression<Func<TEntity, bool>>? filter = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
     {
         IQueryable<TEntity> queryable = Query
@@ -86,6 +242,49 @@ public class EFRepositoryBase<TEntity, TEntityIdType, TContext>(TContext context
         return await queryable.SingleOrDefaultAsync(cancellationToken);
     }
 
+    /// <summary>
+    ///     <b>Asynchronously</b> creates a <see cref="Paginate{T}"/> from an <see cref="IQueryable{T}" /> by enumerating it
+    ///     asynchronously.
+    /// </summary>
+    /// 
+    /// <param name="filter">
+    ///     Represents the linq query which will modify the default query
+    /// </param>
+    /// 
+    /// <param name="include">
+    ///     Represents the linq query which will modify the query to include records of given related entities.
+    /// </param>
+    /// 
+    /// <param name="orderBy">
+    ///     Represents the linq query which will modify the query to sort the sequence with given key.
+    /// </param>
+    /// 
+    /// <param name="index">
+    ///     Represents the startin index which will be fetched data from paginated list
+    /// </param>
+    /// 
+    /// <param name="size">
+    ///     Represents the maximum number of data which will be fetched from paginated list
+    /// </param>
+    /// 
+    /// <param name="withDeleted">
+    ///     Represents the boolean flag which will determine whether to fetch soft deleted records
+    /// </param>
+    /// 
+    /// <param name="enableTracking">
+    ///     Represents the boolean flag which will determine whether to let EF Core to 
+    ///     track the entity of the current context
+    /// </param>
+    /// 
+    /// <param name="cancellationToken">
+    ///     Represents the CancellationToken wihch will be used to facilitate the cancellation of operation.
+    /// </param>
+    /// 
+    /// <returns>
+    ///     A task that represents the asynchronous Paginate operation. 
+    ///     The task result contains the list of paginated items of <typeparamref name="TEntity"/> 
+    ///     that contains elements from the input sequence.
+    /// </returns>
     public async Task<Paginate<TEntity>> GetListAsync(Expression<Func<TEntity, bool>>? filter = null, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, int index = 0, int size = 10, bool withDeleted = false, bool enableTracking = true, CancellationToken cancellationToken = default)
     {
         IQueryable<TEntity> queryable = Query
@@ -99,6 +298,21 @@ public class EFRepositoryBase<TEntity, TEntityIdType, TContext>(TContext context
         return await queryable.PaginateAsync(index, size, cancellationToken);
     }
 
+    /// <summary>
+    ///     <b>Asynchronously</b> performs <see cref="EntityFrameworkCore"/> operations to <b>Update</b> 
+    ///     an <paramref name="entity"/> on connected database while also assigning the update date of the record.
+    ///     Update operation itself is not asynchronous, but the writing changes process is asynchronous.
+    /// </summary>
+    /// 
+    /// <param name="entity">
+    ///     Represents the entity with the identity type of <see cref="TEntityIdType"/> 
+    ///     on which the operation will be performed.
+    /// </param>
+    /// 
+    /// <returns>
+    ///     A task that represents the asynchronous operation. 
+    ///     The task result contains the entity on which the operation was performed.
+    /// </returns>
     public async Task<TEntity> UpdateAsync(TEntity entity)
     {
         entity.UpdatedDate = DateTime.UtcNow;
@@ -107,6 +321,21 @@ public class EFRepositoryBase<TEntity, TEntityIdType, TContext>(TContext context
         return entity;
     }
 
+    /// <summary>
+    ///     <b>Asynchronously</b> performs <see cref="EntityFrameworkCore"/> operations to <b>Update</b> 
+    ///     a list of <paramref name="entities"/> on connected database while also assigning the update date of the record.
+    ///     Update operation itself is not asynchronous, but the writing changes process is asynchronous.
+    /// </summary>
+    /// 
+    /// <param name="entities">
+    ///     Represents the list of entities with the identity type of <see cref="TEntityIdType"/> 
+    ///     on which the operation will be performed.
+    /// </param>
+    /// 
+    /// <returns>
+    ///     A task that represents the asynchronous operation. 
+    ///     The task result contains the list of entities on which the operation was performed.
+    /// </returns>
     public async Task<ICollection<TEntity>> UpdateRangeAsync(ICollection<TEntity> entities)
     {
         foreach (var entity in entities)
